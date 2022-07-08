ecb572b932bb0640ef4ddc659ff3d47faf1fe5c7
djballowe <djballowe@gmail.com>
2022-07-08 13:35:51 -0700

    added the data objects for products under data.js

---
+++ src/data.js
@@ -0,0 +1,104 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    {
+        title: "Bamboo Tooth Brush 10 Pack",
+        price: 14.95,
+        sold_out: false,
+        status: {
+            rating: 5,
+            reviews: 7
+        }
+        
+    },
+
+    {
+        title: "Organic Soap",
+        price: 9.95,
+        sold_out: false,
+        status: {
+            rating: 4.7,
+            reviews: 10
+        }
+    },
+    
+    {
+        title: "Organic Scrub",
+        price: 9.95,
+        sold_out: false,
+        status: {
+            rating: 5,
+            reviews: 11
+        }
+    },
+
+    {
+        title: "Bamboo Q-tips",
+        price: 5.49,
+        sold_out: false,
+        status: {
+            rating: 4.9,
+            review: 5
+        }
+    },
+
+    {
+        title: "All Natural Shampoo",
+        price: 17.95,
+        sold_out: false,
+        status: {
+            rating: 5,
+            review: 15
+        }
+    },
+
+    {
+        title: "Bath Salts",
+        price: 9.95,
+        sold_out: false,
+        status: {
+            rating: 4.5,
+            review: 10
+        }
+    },
+
+    {
+        title: "Eye Drops",
+        price: 17.95,
+        sold_out: false,
+        status: {
+            rating: 4.8,
+            review: 4
+        }
+    },
+
+    {
+        title: "Bamboo Scrub Brush",
+        price: 9.95,
+        sold_out: false,
+        status: {
+            rating: 5,
+            review: 8
+        }
+    },
+
+    {
+        title: "All Natural Face Cream",
+        price: 15.95,
+        sold_out: false,
+        status: {
+            rating: 4.9,
+            review: 10
+        }
+    },
+
+    {
+        title: "Essential Oils Bundle",
+        price: 24.95,
+        sold_out: false,
+        status: {
+            rating: 5,
+            review: 21
+        }
+
+    }
+]
