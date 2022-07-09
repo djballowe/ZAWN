8597db7825524a16d5b421ec79b3f2e084c72508
djballowe <djballowe@gmail.com>
2022-07-09 14:54:07 -0700

    starting individual product pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './styles/App.css';
 import Header from './components/Header';
 import ProductPage from './components/ProductPage';
+import ProductMain from './components/ProductMain';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ProductPage />
+      {/* <ProductPage /> */}
+      <ProductMain />
+
     </div>
   );
 }
--- src/components/ProductMain.js
+++ src/components/ProductMain.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import data from "../data.js"
 
 function ProductMain() {
       
     return (
-      <div>
+      <div className='individual-container'>
+        <div className='image'>
+            <img src={require(`../Images/${data[0].src}`)} alt="" />
+        </div>
+        <div className='title'>
+            <p>{data[0].title}</p>
+            <p>{data[0].price}</p>
+        </div>
+        <p>{data[0].description}</p>
+        <div className='reviews'>
+            <p>{data[0].status.review} / 5</p>
+            <p>{data[0].status.reviews} Reviews</p>
+        </div>
+        <div>
+            <p>Color</p>
+            <div className='color-picker'>
+                <div className='color'></div>
+                <p>White</p>
+            </div>
+        </div>
+        <div className='details'>
 
+        </div>
+        <button>Add to Cart</button>
+        <div className='promise'>
+            <p>Made to Order</p>
+            <p>60 Day Guarantee</p>
+            <p>24/7 Customer Support</p>
+        </div>
       </div>
     )
   
