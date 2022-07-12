00d8b31739f5207bc79cdd4c1e21b7918284dc05
djballowe <djballowe@gmail.com>
2022-07-12 10:03:39 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./styles/App.css";
 import Header from "./components/Header";
+import Home from "./components/Home"
 import ProductPage from "./components/ProductPage";
 import ProductMain from "./components/ProductMain";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="content">
-        <ProductPage />
-        {/* <ProductMain /> */}
-      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,50 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Error from "./Error";
+import ProductPage from "./ProductPage";
 import Icon from "@mdi/react";
-import { mdiMenu } from "@mdi/js";
+import { mdiCar, mdiMenu } from "@mdi/js";
+import Cart from "@mdi/react"
+import { mdiWalletTravel } from '@mdi/js'; 
 import Logo from "../Images/wave.png";
 
 function Header() {
   return (
-    <header>
-      <div className="logo">
-        {/* <img src={Logo} alt="" /> */}
-        <h2>ZAWN</h2>
-      </div>
-      <div className="nav-container">
-        <div className="mobile-menu">
-          <Icon path={mdiMenu} title="mobileMenu" size={1.4} />
+    <Router>
+      <header>
+        <div className="logo">
+          {/* <img src={Logo} alt="" /> */}
+          <h2>ZAWN</h2>
         </div>
-        <ul className="nav">
-          <li>Home</li>
-          <li>Collection</li>
-          <li>About</li>
-        </ul>
-      </div>
-    </header>
+        <div className="nav-container">
+          <div className="mobile-menu">
+            <Icon path={mdiMenu} title="mobileMenu" size={1.4} />
+          </div>
+          <ul className="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/product">Collection</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/cart"><Cart path={mdiWalletTravel} size={1} /></Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
