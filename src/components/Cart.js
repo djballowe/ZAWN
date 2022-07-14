47a6f2fb12846a7f44ecc29b151365692df25131
djballowe <djballowe@gmail.com>
2022-07-14 16:57:41 -0700

    styling cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import CartIcon from "@mdi/react";
+import { mdiWalletTravel } from "@mdi/js";
 
 export default function Cart() {
   return (
     <div className='cart-container'>
         <div className='cart-body'>
-
+            <div className='cart-top'>
+                <div className='bag'>
+                    <CartIcon path={mdiWalletTravel} size={1} />
+                    <p>1 ITEM</p>
+                </div>
+                <button>X</button>
+            </div>
         </div>
     </div>
   )
