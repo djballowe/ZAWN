26346d1295f7dd7b851c9b9bcac453b30a85a378
djballowe <djballowe@gmail.com>
2022-07-15 13:06:43 -0700

    cart

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -5,6 +5,14 @@ import Minus from "@mdi/react";
 import { mdiMinus } from "@mdi/js";
 
 export default function CartItems(props) {
+  const handleClick = (e) => {
+    console.log(e.target.name);
+    e.target.id === "+"
+      ? (props[e.target.name].price = props[e.target.name].price * 2)
+      : (props[e.target.name].price =
+          props[e.target.name].price - props[e.target.name].price);
+  };
+
   return (
     <div className="item-full">
       <div className="cart-image">
@@ -17,12 +25,22 @@ export default function CartItems(props) {
           <p>${props.price}</p>
         </div>
         <div className="quantity">
-          <button className="plus-btn">
-            <Plus path={mdiPlus} size={0.7} />
+          <button
+            className="plus-btn"
+            onClick={handleClick}
+            id="+"
+            name={props.item}
+          >
+            <Plus path={mdiPlus} size={0.7} id="+" name={props.id} />
           </button>
           <input type="text" name="name" placeholder="1" />
-          <button className="minus-btn">
-            <Minus path={mdiMinus} size={0.7} />
+          <button
+            className="minus-btn"
+            id="-"
+            onClick={handleClick}
+            name={props.item}
+          >
+            <Minus path={mdiMinus} size={0.7} name={props.id} id="-" />
           </button>
         </div>
       </div>
