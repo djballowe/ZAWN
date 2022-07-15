ac1327d9399cd78c0bbade1ae449dbdb3236f87c
djballowe <djballowe@gmail.com>
2022-07-15 10:22:54 -0700

    adding multiple car items works

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Plus from "@mdi/react"
-import { mdiPlus } from '@mdi/js';
-import Minus from '@mdi/react'
-import { mdiMinus } from '@mdi/js';  
+import Plus from "@mdi/react";
+import { mdiPlus } from "@mdi/js";
+import Minus from "@mdi/react";
+import { mdiMinus } from "@mdi/js";
 
 export default function CartItems(props) {
   return (
@@ -11,16 +11,18 @@ export default function CartItems(props) {
         <img src={require(`../Images/${props.src}`)} alt="" />
       </div>
       <div className="item-container">
-        <h3>{props.item}</h3>
-        <p>Forest Green</p>
-        <p>${props.price}</p>
+        <div className="item-text">
+          <h3>{props.item}</h3>
+          <p>Forest Green</p>
+          <p>${props.price}</p>
+        </div>
         <div className="quantity">
           <button className="plus-btn">
-            <Plus path={mdiPlus} size={.7} />
+            <Plus path={mdiPlus} size={0.7} />
           </button>
           <input type="text" name="name" placeholder="1" />
           <button className="minus-btn">
-            <Minus path={mdiMinus} size={.7}/>
+            <Minus path={mdiMinus} size={0.7} />
           </button>
         </div>
       </div>
