2d1a405fcc08465ac85ecf247f89d4ff0ed86f1e
djballowe <djballowe@gmail.com>
2022-07-23 13:51:20 -0700

    adding checkout styles

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -4,6 +4,8 @@ import PayPal from "../Images/Payment pngs/paypal.png";
 import ApplePay from "../Images/Payment pngs/applepay.png";
 import Amazon from "../Images/Payment pngs/amazon.png";
 import { countryList } from "../CountryData";
+import Arrow from "../Images/arrow.png";
+import whiteArrow from "../Images/chevron-right.png";
 
 export default function Checkout() {
   const options = countryList.map((country) => {
@@ -21,101 +23,112 @@ export default function Checkout() {
           <h2>ZAWN</h2>
           <img src={Logo} alt="" />
         </div>
-        <div className="express">
-          <p>Express Checkout</p>
-          <div className="payment-buttons">
-            <button>
-              <img src={Amazon} alt="" />
-            </button>
-            <button>
-              <img src={PayPal} alt="" />
-            </button>
-            <button>
-              <img src={ApplePay} alt="" />
-            </button>
+        <div className="checkout-cart">
+          <div className="show-order">
+            <p>Show order summary</p>
+            <img src={Arrow} alt="" />
           </div>
         </div>
-        <div className="divider">
-          <div></div>
-          <p>OR</p>
-          <div></div>
-        </div>
-        <div className="contact-info">
-          <h3>Contact Information</h3>
-          <p>
-            Already have an account? <a href="">Log in</a>
-          </p>
-        </div>
-        <div className="checkout-email">
-          <input type="Email" placeholder="Email" required />
-          <div className="checkbox">
-            <input type="checkbox" />
-            <p>Email me with news and offers</p>
-          </div>
-        </div>
-        <div className="checkout-shipping">
-          <h2>Shipping address</h2>
-          <form action="">
-            <select name="" id="">
-              {options}
-            </select>
-            <div className="name">
-              <input type="text" placeholder="First name" />
-              <input type="text" placeholder="Last name" />
+        <div className="checkout-cart-items">
+          <div className="cart-item-checkout">
+            <img
+              src="../Images/Product Page/Tooth Brush/ToothBrush2.jpg"
+              alt=""
+            />
+            <div>
+              <p>Item</p>
+              <p>color</p>
             </div>
-            <input type="text" placeholder="Address" />
-            <input type="text" placeholder="Apartment, suite, ect" />
-            <div className="address">
-              <input type="text" placeholder="City" />
-              <input type="text" placeholder="State" />
-              <input type="text" placeholder="ZIP code" />
-            </div>
-            <input type="text" placeholder="Phone" />
-            <div className="text">
-              <input type="checkbox" />
-              <p>Text me with news and offers</p>
-            </div>
-          </form>
-        </div>
-        <div>
-          <p>Return to cart</p>
-          <button>Continue to Shipping</button>
-        </div>
-      </div>
-      <div className="checkout-cart-items">
-        <div>
-          <img src="" alt="" />
-          <div>
-            <p>Item</p>
-            <p>color</p>
-          </div>
-          <p>$64.00</p>
-        </div>
-        <div>
-          <input type="text" placeholder="Gift card or discount code" />
-          <button>Apply</button>
-        </div>
-        <div>
-          <div>
-            <p>Subtotal</p>
             <p>$64.00</p>
           </div>
-          <div>
-            <p>Shipping</p>
-            <p>$16.44</p>
+          <div className="coupon-code">
+            <input type="text" placeholder="Gift card or discount code" />
+            <button>
+              <img src={whiteArrow} alt="" />
+            </button>
           </div>
-          <div>
-            <p>Taxes</p>
-            <p>$4.64</p>
+          <div className="checkout-subtotal-container">
+            <div className="checkout-subtotal">
+              <p>Subtotal</p>
+              <p>$64.00</p>
+            </div>
+            <div className="checkout-shipping">
+              <p>Shipping</p>
+              <p>$16.44</p>
+            </div>
+            <div className="checkout-tax">
+              <p>Taxes</p>
+              <p>$4.64</p>
+            </div>
+          </div>
+          <div className="checkout-total">
+            <p>Total</p>
+            <div className="currency">
+              <p>USD</p>
+              <p>$85.08</p>
+            </div>
           </div>
         </div>
-        <div>
-          <p>Total</p>
-          <div>
-            <p>USD</p>
-            <p>$85.08</p>
-          </div>
+      </div>
+      <div className="express">
+        <p>Express Checkout</p>
+        <div className="payment-buttons">
+          <button>
+            <img src={Amazon} alt="" />
+          </button>
+          <button>
+            <img src={PayPal} alt="" />
+          </button>
+          <button>
+            <img src={ApplePay} alt="" />
+          </button>
         </div>
+      </div>
+      <div className="divider">
+        <div></div>
+        <p>OR</p>
+        <div></div>
+      </div>
+      <div className="contact-info">
+        <h3>Contact Information</h3>
+        <p>
+          Already have an account? <a href="">Log in</a>
+        </p>
+      </div>
+      <div className="checkout-email">
+        <input type="Email" placeholder="Email" required />
+        <div className="checkbox">
+          <input type="checkbox" />
+          <p>Email me with news and offers</p>
+        </div>
+      </div>
+      <div className="checkout-shipping-info">
+        <h2>Shipping address</h2>
+        <form action="">
+          <select name="" id="">
+            {options}
+          </select>
+          <div className="name">
+            <input type="text" placeholder="First name" required />
+            <input type="text" placeholder="Last name" required />
+          </div>
+          <input type="text" placeholder="Address" required />
+          <input type="text" placeholder="Apartment, suite, ect" required />
+          <div className="address">
+            <input type="text" placeholder="City" required />
+            <input type="text" placeholder="State" required />
+            <input type="text" placeholder="ZIP code" required />
+          </div>
+          <input type="text" placeholder="Phone" required />
+          <div className="text">
+            <input type="checkbox" />
+            <p>Text me with news and offers</p>
+          </div>
+        </form>
+      </div>
+      <div className="continue">
+        <button>Continue to Shipping</button>
+        <p>Return to cart</p>
       </div>
     </div>
   );
