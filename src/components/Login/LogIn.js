5b0e8bca6d15f57ec974fba9a8b8683b3f3085ca
djballowe <djballowe@gmail.com>
2022-07-28 10:01:09 -0700

    registering email passwords works on correct form

--- src/components/Login/LogIn.js
+++ src/components/Login/LogIn.js
@@ -82,46 +82,105 @@ const LogIn = () => {
 };
 
 export const Register = () => {
+  const [isEmail, setIsEmail] = useState("");
+  const [isPassword, setIsPassword] = useState("");
+  const [isConfirmPassword, setIsConfirmPassword] = useState("");
+  const [user] = useAuthState(auth);
+
   let navigate = useNavigate();
   const handleClick = () => {
     navigate("/login");
   };
-  return (
-    <div className="login-container">
-      <div className="login-text">
-        <h2>Register</h2>
-        <p>Please fill in the fields below:</p>
+
+  const handleChange = (e) => {
+    if (e.target.id === "email") {
+      setIsEmail(e.target.value);
+    } else if (e.target.id === "password") {
+      setIsPassword(e.target.value);
+    } else if (e.target.id === "confirm-password") {
+      setIsConfirmPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isConfirmPassword !== isPassword) {
+      alert("Passwords must match");
+    } else if (isPassword.length < 8) {
+      alert("Password must be at least 8 characters");
+    } else {
+      signInWithEmail(isEmail, isPassword);
+    }
+  };
+  if (user) {
+    return <AccountPage />;
+  } else
+    return (
+      <div className="login-container">
+        <div className="login-text">
+          <h2>Register</h2>
+          <p>Please fill in the fields below:</p>
+        </div>
+        <div className="contact-inputs">
+          <form action="" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="First Name"
+              id="first-name"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              id="last-name"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="email"
+              placeholder="E-mail"
+              id="email"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              id="confirm-password"
+              onChange={handleChange}
+              required
+            />
+            <button type="submit">Create Account</button>
+          </form>
+        </div>
+        <div className="divider">
+          <div></div>
+          <p>OR</p>
+          <div></div>
+        </div>
+        <div className="google">
+          <button
+            onClick={() => {
+              navigate("/account");
+            }}
+          >
+            Sign up with <img src={Google} alt="" />
+          </button>
+        </div>
+        <div className="new-customer">
+          <p>Already have an account?</p>
+          <p onClick={handleClick}>Log in</p>
+        </div>
       </div>
-      <div className="contact-inputs">
-        <form action="">
-          <input type="text" placeholder="First Name" id="first-name" required />
-          <input type="text" placeholder="Last Name" id="last-name" required />
-          <input type="email" placeholder="E-mail" id="email" required />
-          <input type="password" placeholder="Password" id="password" required />
-          <input type="password" placeholder="Confirm Password" id="confirm-password" required />
-          <button type="submit">Create Account</button>
-        </form>
-      </div>
-      <div className="divider">
-        <div></div>
-        <p>OR</p>
-        <div></div>
-      </div>
-      <div className="google">
-        <button
-          onClick={() => {
-            navigate("/account");
-          }}
-        >
-          Sign up with <img src={Google} alt="" />
-        </button>
-      </div>
-      <div className="new-customer">
-        <p>Already have an account?</p>
-        <p onClick={handleClick}>Log in</p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LogIn;
